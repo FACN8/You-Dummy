824df48b4f14cc2c43822f9d8a59bdb7148ff834
MahmodMhamed <mahmod.mhamid@hotmail.com>
2020-02-19 13:37:59 +0200

    added client-side validation to registration page

--- public/index.js
+++ public/index.js
@@ -1,1 +1,36 @@
 /*--- This file for client-side validation ---*/
+
+(function() {
+  const form = document.querySelector('form');
+  const username = document.querySelector('input[name=username]');
+  const password = document.querySelector('input[name=password]');
+  const confPassword = document.querySelector('input[name=confPassword]');
+  const submitBtn = document.querySelector('input[type=submit]');
+  const errorMessage = document.querySelector('span');
+
+  submitBtn.disabled = true;
+  errorMessage.style.visibility = 'hidden';
+
+  form.oninput = () => {
+    if (username.value !== '' && password.value !== '' && confPassword.value !== '') {
+      submitBtn.disabled = false;
+    } else {
+      submitBtn.disabled = true;
+    }
+
+    if (password.value === confPassword.value) {
+      errorMessage.style.visibility = 'hidden';
+    }
+  };
+
+  submitBtn.addEventListener('click', e => {
+    e.preventDefault();
+    if (password.value != confPassword.value) {
+      errorMessage.style.visibility = 'visible ';
+    } else {
+      setTimeout(() => {
+        window.location = '/login';
+      }, 1200);
+    }
+  });
+})();
